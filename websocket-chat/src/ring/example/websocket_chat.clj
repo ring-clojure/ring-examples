e3860bdf1f6fdf5710faff872018ff4ebe89de93
James Reeves <jreeves@weavejester.com>
2024-05-02 01:04:40 +0100

    Add Reitit and static resources to chat example

--- websocket-chat/src/ring/example/websocket_chat.clj
+++ websocket-chat/src/ring/example/websocket_chat.clj
@@ -1,10 +1,18 @@
 (ns ring.example.websocket-chat
-  (:require [ring.adapter.jetty :as adapter]))
+  (:require [reitit.ring :as rr]
+            [ring.adapter.jetty :as adapter]))
 
-(defn handler [_request]
+(defn chat-handler [_request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "<h1>Hello World</h1>"})
 
+(def app-handler
+  (rr/ring-handler
+   (rr/router ["/chat" chat-handler])
+   (rr/routes
+    (rr/create-resource-handler {:path "/"})
+    (rr/create-default-handler))))
+
 (defn run-server [options]
-  (adapter/run-jetty handler options))
+  (adapter/run-jetty app-handler options))
